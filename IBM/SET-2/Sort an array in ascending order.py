ce5cbd54bc5b00e662cafd3745d13b3f686e449f
Charan Sai Reddy W <72861534+charanwudaru@users.noreply.github.com>
2023-09-13 12:18:43 +0530

    Add files via upload

---
+++ IBM/SET-2/Sort an array in ascending order.py
@@ -0,0 +1,9 @@
+arr=[4,64,7,1,5,3]
+n=len(arr)
+for i in range(n-1):
+    for j in range(i+1,n):
+        if arr[i]>arr[j]:
+            arr[i],arr[j]=arr[j],arr[i]
+
+print("Sorted array is:",arr)
+
---
+++ IBM/SET-2/reverse arr.py
@@ -0,0 +1,3 @@
+arr=[4,64,7,1,5,3]
+arr.reverse()
+print(arr)
